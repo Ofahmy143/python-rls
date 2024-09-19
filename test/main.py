5b918276bc0acb329cb3bb3775476b84639373c8
Omar <102324541+Ofahmy143@users.noreply.github.com>
2024-09-19 10:27:59 +0300

    fix: remove userId from test route in main.py

--- test/main.py
+++ test/main.py
@@ -9,8 +9,8 @@ from test.models import Item
 app = FastAPI()
 
 
-@app.get("/users/{user_id}/items")
-async def get_users(user_id, db: AsyncSession = Depends(get_async_session)):
+@app.get("/users/items")
+async def get_users(db: AsyncSession = Depends(get_async_session)):
     stmt = select(Item)
     result = await db.execute(stmt)
     items = result.scalars().all()
